7d731111c13196d5458427862e3ab93f30d080cc
klc213bk <klc213bk@gmail.com>
2021-04-27 12:05:39 +0800

    initial

--- pcr420669-load/src/main/java/com/transglobe/streamingetl/pcr420669/load/InitialLoadApp.java
+++ pcr420669-load/src/main/java/com/transglobe/streamingetl/pcr420669/load/InitialLoadApp.java
@@ -16,6 +16,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +27,13 @@ public class InitialLoadApp {
 	private static final Logger logger = LoggerFactory.getLogger(InitialLoadApp.class);
 
 	private static final String CONFIG_FILE_NAME = "config.properties";
+	
+	private static final int THREADS = 5;
+	
+	private static final long SEQ_INTERVAL = 100000L;
+
+	private BasicDataSource sourceConnectionPool;
+	private BasicDataSource sinkConnectionPool;
 
 	static class LoadBean {
 		String fullTableName;
@@ -37,6 +45,34 @@ public class InitialLoadApp {
 
 	public InitialLoadApp(String fileName) throws Exception {
 		config = Config.getConfig(CONFIG_FILE_NAME);
+
+		sourceConnectionPool = new BasicDataSource();
+		 
+		sourceConnectionPool.setUrl(config.sourceDbUrl);
+		sourceConnectionPool.setUsername(config.sourceDbUsername);
+		sourceConnectionPool.setPassword(config.sourceDbPassword);
+		sourceConnectionPool.setDriverClassName(config.sourceDbDriver);
+		sourceConnectionPool.setMaxTotal(THREADS);
+		
+		sinkConnectionPool = new BasicDataSource();
+		sinkConnectionPool.setUrl(config.sinkDbUrl);
+		sinkConnectionPool.setUsername(config.sinkDbUsername);
+		sinkConnectionPool.setPassword(config.sinkDbPassword);
+		sinkConnectionPool.setDriverClassName(config.sinkDbDriver);
+		sinkConnectionPool.setMaxTotal(THREADS);
+
+	}
+	private void close() {
+		try {
+			if (sourceConnectionPool != null) sourceConnectionPool.close();
+		} catch (Exception e) {
+			logger.error("message={}, stack trace={}", e.getMessage(), ExceptionUtils.getStackTrace(e));
+		}
+		try {
+			if (sinkConnectionPool != null) sinkConnectionPool.close();
+		} catch (Exception e) {
+			logger.error("message={}, stack trace={}", e.getMessage(), ExceptionUtils.getStackTrace(e));
+		}
 	}
 	public static void main(String[] args) {
 		logger.info(">>> start run InitialLoadApp");
@@ -51,8 +87,11 @@ public class InitialLoadApp {
 			//			logger.info(" test1 span={}", (System.currentTimeMillis() - t0));
 			//
 			t0 = System.currentTimeMillis();
-			app.test4();
-			logger.info(" test3 span={}", (System.currentTimeMillis() - t0));
+			app.test1();
+			
+			app.close();
+			
+			logger.info(" test1 span={}", (System.currentTimeMillis() - t0)); //  test4 span=544347
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -62,7 +101,7 @@ public class InitialLoadApp {
 
 	private void test2() throws Exception {
 		logger.info(">>> run test2");
-		ExecutorService executor = Executors.newFixedThreadPool(3);
+		ExecutorService executor = Executors.newFixedThreadPool(THREADS);
 		try {
 
 			List<String> fulleSourceTableNames = new ArrayList<>();
@@ -239,11 +278,7 @@ public class InitialLoadApp {
 			// truncate table
 			Statement stmt = sourceConn.createStatement();
 			ResultSet resultSet = stmt.executeQuery(
-					"select 1 as ROLE_TYPE,a.LIST_ID,a.POLICY_ID,a.NAME,a.CERTI_CODE,a.MOBILE_TEL,a.EMAIL,a.ADDRESS_ID,b.ADDRESS_1 from t_policy_holder a inner join t_address b on a.address_id = b.address_id \n" + 
-							" union \n" + 
-							" select 2 as ROLE_TYPE,a.LIST_ID,a.POLICY_ID,a.NAME,a.CERTI_CODE,a.MOBILE_TEL,a.EMAIL,a.ADDRESS_ID,b.ADDRESS_1 from t_insured_list a inner join t_address b on a.address_id = b.address_id \n" + 
-							" union \n" + 
-					"  select 3 as ROLE_TYPE,a.LIST_ID,a.POLICY_ID,a.NAME,a.CERTI_CODE,a.MOBILE_TEL,a.EMAIL,a.ADDRESS_ID,b.ADDRESS_1 from t_contract_bene a inner join t_address b on a.address_id = b.address_id \n" );
+					"select 1 as ROLE_TYPE,a.LIST_ID,a.POLICY_ID,a.NAME,a.CERTI_CODE,a.MOBILE_TEL,a.EMAIL,a.ADDRESS_ID,b.ADDRESS_1 from t_policy_holder a inner join t_address b on a.address_id = b.address_id" );
 
 
 			List<InterestedPartyContact> contactList = new ArrayList<>();
@@ -265,6 +300,8 @@ public class InitialLoadApp {
 			resultSet.close();
 			stmt.close();
 
+			logger.error(">>> query completed");
+			
 			// save to sink db
 			int count = 0;
 			sinkConn = DriverManager.getConnection(config.sinkDbUrl, config.sinkDbUsername, config.sinkDbPassword);
@@ -318,7 +355,7 @@ public class InitialLoadApp {
 
 				pstmt.addBatch();
 
-				if (count % 500 == 0 || count == contactList.size()) {
+				if (count % 1000 == 0 || count == contactList.size()) {
 					pstmt.executeBatch();//executing the batch  
 
 					logger.info("   >>>count={}, execute batch", count);
@@ -346,7 +383,7 @@ public class InitialLoadApp {
 		try {
 
 			Long maxSeq = 8935503221L;
-		//	Long seq = 10000000L;
+			//	Long seq = 10000000L;
 			Long beginSeq = 0L;
 			Long intervalSeq = 100000L;
 			List<LoadBean> loadBeanList = new ArrayList<>();
@@ -357,7 +394,7 @@ public class InitialLoadApp {
 				loadBean.startSeq = beginSeq;
 				loadBean.endSeq = beginSeq + intervalSeq;
 				loadBeanList.add(loadBean);
-				
+
 				beginSeq = beginSeq + intervalSeq;
 			}
 			LoadBean loadBean = new LoadBean();
@@ -370,11 +407,11 @@ public class InitialLoadApp {
 			//			fulleSourceTableNames.add(config.sourceTableInsuredList);
 			//			fulleSourceTableNames.add(config.sourceTableContractBene);
 
-//			Map<String, Integer> roleTypeMap = new HashMap<>();
-//			roleTypeMap.put(config.sourceTablePolicyHolder, 1);
-//			roleTypeMap.put(config.sourceTableInsuredList, 2);
-//			roleTypeMap.put(config.sourceTableContractBene, 3);
-			
+			//			Map<String, Integer> roleTypeMap = new HashMap<>();
+			//			roleTypeMap.put(config.sourceTablePolicyHolder, 1);
+			//			roleTypeMap.put(config.sourceTableInsuredList, 2);
+			//			roleTypeMap.put(config.sourceTableContractBene, 3);
+
 			List<CompletableFuture<Map<String, String>>> futures = 
 					loadBeanList.stream().map(t -> CompletableFuture.supplyAsync(
 							() -> loadInterestedPartyContact3(t.fullTableName, t.roleType, t.startSeq, t.endSeq), executor))
@@ -417,7 +454,7 @@ public class InitialLoadApp {
 			String sql = "select " + roleType + " as ROLE_TYPE,a.LIST_ID,a.POLICY_ID,a.NAME,a.CERTI_CODE,a.MOBILE_TEL,a.EMAIL,a.ADDRESS_ID,b.ADDRESS_1 from " + sourceTableName + " a inner join " + config.sourceTableAddress + " b on a.address_id = b.address_id "
 					+ " where " + startSeq + " <= a.address_id and a.address_id < " + endSeq;
 
-			logger.info(">>> sql= {}",sql);
+			//	logger.info(">>> sql= {}",sql);
 
 			Statement stmt = sourceConn.createStatement();
 			ResultSet resultSet = stmt.executeQuery(sql);
@@ -445,7 +482,7 @@ public class InitialLoadApp {
 			resultSet.close();
 			stmt.close();
 
-			logger.info("query completed for table {}",sourceTableName);
+			//	logger.info("query completed for table {}",sourceTableName);
 
 			// save to sink db
 			int count = 0;
@@ -507,7 +544,7 @@ public class InitialLoadApp {
 				}
 			}
 			logger.info("   >>>count={}, startSeq={},endSeq={} ", count, startSeq, endSeq);
-			
+
 			if (pstmt != null) pstmt.close();
 
 			sinkConn.commit(); 
@@ -540,23 +577,23 @@ public class InitialLoadApp {
 	}
 	private Map<String, String> loadInterestedPartyContact4(String sourceTableName, Integer roleType, Long startSeq, Long endSeq){
 		logger.info(">>> run loadInterestedPartyContact, table={}, roleType={}", sourceTableName, roleType);
-		Connection sourceConn = null;
-		Connection sinkConn = null;
-
+		
 		Map<String, String> map = new HashMap<>();
 		try {
-			Class.forName(config.sourceDbDriver);
-			sourceConn = DriverManager.getConnection(config.sourceDbUrl, config.sourceDbUsername, config.sourceDbPassword);
-	
+			//			Class.forName(config.sourceDbDriver);
+			//			sourceConn = DriverManager.getConnection(config.sourceDbUrl, config.sourceDbUsername, config.sourceDbPassword);
+
+			Connection sourceConn = this.sourceConnectionPool.getConnection();
+			Connection sinkConn = this.sinkConnectionPool.getConnection();
+			
 			String sql = "select " + roleType + " as ROLE_TYPE,a.LIST_ID,a.POLICY_ID,a.NAME,a.CERTI_CODE,a.MOBILE_TEL,a.EMAIL,a.ADDRESS_ID,b.ADDRESS_1 from " + sourceTableName + " a inner join " + config.sourceTableAddress + " b on a.address_id = b.address_id "
 					+ " where " + startSeq + " <= a.address_id and a.address_id < " + endSeq;
-	
-			logger.info(">>> sql= {}",sql);
-	
+
+			logger.info(">>> sql= {}", sql);
+
 			Statement stmt = sourceConn.createStatement();
 			ResultSet resultSet = stmt.executeQuery(sql);
-	
-			sinkConn = DriverManager.getConnection(config.sinkDbUrl, config.sinkDbUsername, config.sinkDbPassword);
+
 			sinkConn.setAutoCommit(false); 
 			PreparedStatement pstmt = sinkConn.prepareStatement(
 					"insert into " + config.sinkTableParty + " (ROLE_TYPE,LIST_ID,POLICY_ID,NAME,CERTI_CODE,MOBILE_TEL,EMAIL,ADDRESS_ID,ADDRESS_1) " 
@@ -564,7 +601,7 @@ public class InitialLoadApp {
 			Long count = 0L;
 			while (resultSet.next()) {
 				count++;
-				
+
 				pstmt.setInt(1, resultSet.getInt("ROLE_TYPE"));
 				Long listId = resultSet.getLong("LIST_ID");
 				Long policyId = resultSet.getLong("POLICY_ID");
@@ -574,10 +611,10 @@ public class InitialLoadApp {
 				String email = resultSet.getString("EMAIL");
 				Long addressId = resultSet.getLong("ADDRESS_ID");
 				String address1 = resultSet.getString("ADDRESS_1");
-				
+
 				pstmt.setLong(2, listId);
 				pstmt.setLong(3, policyId);
-				
+
 				if (name == null) {
 					pstmt.setNull(4, Types.VARCHAR);
 				} else {
@@ -604,28 +641,32 @@ public class InitialLoadApp {
 				} else {
 					pstmt.setString(9, address1);
 				}
-	
+
 				pstmt.addBatch();
-				
+
 				if (count % 1000 == 0) {
 					pstmt.executeBatch();//executing the batch  
-	
-					logger.info("   >>>count={}, execute batch", count);
+					sinkConn.commit(); 
+					logger.info("   >>>startSeq={}, count={}, address_id={} execute batch committed ", startSeq, count, addressId);
+					pstmt.clearBatch();
 				}
 			}
-			logger.info("   >>>count={}, startSeq={},endSeq={} ", count, startSeq, endSeq);
+
 			pstmt.executeBatch();
 			if (pstmt != null) pstmt.close();
 			if (count > 0) sinkConn.commit(); 
-			
+
 			resultSet.close();
 			stmt.close();
 
+			sourceConn.close();
+			sinkConn.close();
+			
 			map.put("RETURN_CODE", "0");
 			map.put("SOURCE_TABLE", sourceTableName);
 			map.put("SINK_TABLE", config.sinkTableParty);
 			map.put("RECORD_COUNT", String.valueOf(count));
-	
+
 		}  catch (Exception e) {
 			logger.error("message={}, stack trace={}", e.getMessage(), ExceptionUtils.getStackTrace(e));
 			map.put("RETURN_CODE", "-999");
@@ -634,81 +675,74 @@ public class InitialLoadApp {
 			map.put("ERROR_MSG", e.getMessage());
 			map.put("STACK_TRACE", ExceptionUtils.getStackTrace(e));
 		} finally {
-			if (sourceConn != null) {
-				try {
-					sourceConn.close();
-				} catch (SQLException e) {
-					logger.error("message={}, stack trace={}", e.getMessage(), ExceptionUtils.getStackTrace(e));
-					map.put("RETURN_CODE", "-999");
-					map.put("ERROR_MSG", e.getMessage());
-					map.put("STACK_TRACE", ExceptionUtils.getStackTrace(e));
-				}
-			}
+
 		}
 		return map;
 	}
 	private void test4() throws Exception {
-	
-			ExecutorService executor = Executors.newFixedThreadPool(11);
-			try {
-	
-				Long maxSeq = 8935503221L;
-			//	Long seq = 10000000L;
-				Long beginSeq = 0L;
-				Long intervalSeq = 100000L;
-				List<LoadBean> loadBeanList = new ArrayList<>();
-				while (beginSeq <= maxSeq) {
-					LoadBean loadBean = new LoadBean();
-					loadBean.fullTableName = config.sourceTablePolicyHolder;
-					loadBean.roleType = 1;
-					loadBean.startSeq = beginSeq;
-					loadBean.endSeq = beginSeq + intervalSeq;
-					loadBeanList.add(loadBean);
-					
-					beginSeq = beginSeq + intervalSeq;
-				}
+
+		ExecutorService executor = Executors.newFixedThreadPool(THREADS);
+		
+		try {
+
+			Connection sourceConn = this.sourceConnectionPool.getConnection();
+			Statement stmt = sourceConn.createStatement();
+			ResultSet resultSet  = stmt.executeQuery("select max(address_id) as MAX_ADDRESS_ID from " + config.sourceTableAddress);
+			Long maxSeq = 0L;
+			while (resultSet.next()) {
+				maxSeq = resultSet.getLong("MAX_ADDRESS_ID");
+			}
+			
+			maxSeq = maxSeq++;
+
+			Long beginSeq = 0L;
+		
+			List<LoadBean> loadBeanList = new ArrayList<>();
+			while (beginSeq <= maxSeq) {
 				LoadBean loadBean = new LoadBean();
 				loadBean.fullTableName = config.sourceTablePolicyHolder;
 				loadBean.roleType = 1;
 				loadBean.startSeq = beginSeq;
-				loadBean.endSeq = maxSeq;
+				loadBean.endSeq = beginSeq + SEQ_INTERVAL;
 				loadBeanList.add(loadBean);
-	
-				//			fulleSourceTableNames.add(config.sourceTableInsuredList);
-				//			fulleSourceTableNames.add(config.sourceTableContractBene);
-	
-	//			Map<String, Integer> roleTypeMap = new HashMap<>();
-	//			roleTypeMap.put(config.sourceTablePolicyHolder, 1);
-	//			roleTypeMap.put(config.sourceTableInsuredList, 2);
-	//			roleTypeMap.put(config.sourceTableContractBene, 3);
-				
-				List<CompletableFuture<Map<String, String>>> futures = 
-						loadBeanList.stream().map(t -> CompletableFuture.supplyAsync(
-								() -> loadInterestedPartyContact4(t.fullTableName, t.roleType, t.startSeq, t.endSeq), executor))
-						.collect(Collectors.toList());
-	
-				List<Map<String, String>> result = futures.stream().map(CompletableFuture::join).collect(Collectors.toList());
-	
-				for (Map<String, String> map : result) {
-					String sourceTable = map.get("SOURCE_TABLE");
-					String sinkTable = map.get("SINK_TABLE");
-					String returnCode = map.get("RETURN_CODE");
-					String recordCount = "";
-					String errormsg = "";
-					String stackTrace = "";
-					if ("0".equals(returnCode)) {
-						recordCount = map.get("RECORD_COUNT");
-					} else {
-						errormsg = map.get("ERROR_MSG");
-						stackTrace = map.get("STACE_TRACE");
-					}
-					logger.info("sourceTable={}, sinkTable={}, returnCode={}, recordCount={}, errormsg={},stackTrace={}", 
-							sourceTable, sinkTable, returnCode, recordCount, errormsg, stackTrace);
-				}
-	
-			} finally {
-				if (executor != null) executor.shutdown();
+
+				beginSeq = beginSeq + SEQ_INTERVAL;
 			}
-	
+			LoadBean loadBean = new LoadBean();
+			loadBean.fullTableName = config.sourceTablePolicyHolder;
+			loadBean.roleType = 1;
+			loadBean.startSeq = beginSeq;
+			loadBean.endSeq = maxSeq;
+			loadBeanList.add(loadBean);
+
+			List<CompletableFuture<Map<String, String>>> futures = 
+					loadBeanList.stream().map(t -> CompletableFuture.supplyAsync(
+							() -> loadInterestedPartyContact4(t.fullTableName, t.roleType, t.startSeq, t.endSeq), executor))
+					.collect(Collectors.toList());
+
+			List<Map<String, String>> result = futures.stream().map(CompletableFuture::join).collect(Collectors.toList());
+
+			for (Map<String, String> map : result) {
+				String sourceTable = map.get("SOURCE_TABLE");
+				String sinkTable = map.get("SINK_TABLE");
+				String returnCode = map.get("RETURN_CODE");
+				String recordCount = "";
+				String errormsg = "";
+				String stackTrace = "";
+				if ("0".equals(returnCode)) {
+					recordCount = map.get("RECORD_COUNT");
+				} else {
+					errormsg = map.get("ERROR_MSG");
+					stackTrace = map.get("STACE_TRACE");
+				}
+				logger.info("sourceTable={}, sinkTable={}, returnCode={}, recordCount={}, errormsg={},stackTrace={}", 
+						sourceTable, sinkTable, returnCode, recordCount, errormsg, stackTrace);
+			}
+			
+		} finally {
+			if (executor != null) executor.shutdown();
+		
 		}
+	}
+
 }
