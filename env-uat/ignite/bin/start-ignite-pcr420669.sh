3b70ed70ecfd5eb3d3d060b6e9e9cedf9f48cf3b
klc213bk <klc213bk@gmail.com>
2021-05-12 17:55:17 +0800

    initial

--- env-uat/ignite/bin/start-ignite-pcr420669.sh
+++ env-uat/ignite/bin/start-ignite-pcr420669.sh
@@ -2,7 +2,7 @@
 
 IGNITE_HOME=/home/kafka/apache-ignite-2.9.1-bin
 
-APP_HOME=/home/kafka/streamingetl-pcr420669
+APP_HOME=/home/kafka/streamingetl-pcr420669/env-uat
 
-${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/env-dev1/ignite/config/pcr420669-ignite.xml
+${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/ignite/config/pcr420669-ignite.xml
 
