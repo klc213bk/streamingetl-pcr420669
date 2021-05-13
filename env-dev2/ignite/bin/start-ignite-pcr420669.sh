be85e9af08362f743bad8749cb1dc0a8e265551a
klc213bk <klc213bk@gmail.com>
2021-05-13 16:40:36 +0800

    initial

--- env-dev2/ignite/bin/start-ignite-pcr420669.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IGNITE_HOME=/home/feib/apache-ignite-2.9.1-bin
-
-APP_HOME=/home/feib/gitrepository/transglobe/streamingetl-pcr420669/env-dev2
-
-${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/ignite/config/pcr420669-ignite.xml
-
