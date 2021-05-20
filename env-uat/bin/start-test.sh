e60578dacadc74ecdfba7a3988984e382e889dcb
klc213bk <klc213bk@gmail.com>
2021-05-20 22:27:21 +0800

    initial

--- env-uat/bin/start-test.sh
+++ env-uat/bin/start-test.sh
@@ -2,7 +2,10 @@
 
 APP_HOME=/home/kafka/streamingetl-pcr420669
 
-java -cp ${APP_HOME}/lib/pcr420669-test-1.0.jar:${APP_HOME}/lib/jackson-core-2.9.1.jar com.transglobe.streamingetl.pcr420669.test.TestApp
+./start-initialload.sh
+
+java -cp "${APP_HOME}/lib/pcr420669-test-1.0.jar:${APP_HOME}/lib/*" com.transglobe.streamingetl.pcr420669.test.TestApp
+
 STATUS=$?	
 if [ ${STATUS} == 0 ]
 then
