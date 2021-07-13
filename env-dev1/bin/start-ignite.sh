883893473b577549ff888158fd6ad246a1cd145f
steven <steven@mylinux>
2021-07-13 17:38:53 +0800

    initial

--- env-dev1/bin/start-ignite.sh
+++ env-dev1/bin/start-ignite.sh
@@ -3,5 +3,5 @@
 IGNITE_HOME=/home/steven/apache-ignite-2.9.1-bin
 APP_HOME=/home/steven/gitrepo/transglobe/streamingetl-pcr420669
 
-${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/env-dev1/config/ignite-pcr420669.xml
+${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/env-dev1/config/ignite-config.xml
 
