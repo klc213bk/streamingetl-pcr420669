6021407986c8933ad5fce38d4d2de2c19a496edf
steven <steven@mylinux>
2021-07-13 10:03:48 +0800

    initial

---
+++ env-uat/bin/start-ignite-pcr420669.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+IGNITE_HOME=/home/kafka/apache-ignite-2.9.1-bin
+APP_HOME=/home/kafka/streamingetl-pcr420669
+
+${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/env-uat/config/ignite-pcr420669.xml
+
