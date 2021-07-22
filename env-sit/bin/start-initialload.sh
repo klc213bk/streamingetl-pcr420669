0e92b7a92c0c9d510006e4fc13afbecae322a54a
steven <steven@mylinux>
2021-07-22 14:28:53 +0800

    initial

--- env-sit/bin/start-initialload.sh
+++ env-sit/bin/start-initialload.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 APP_HOME=/home/kafka/streamingetl-pcr420669
+STREAMINGETL_HOME=/home/kafka/streamingetl
+
+cd ${STREAMINGETL_HOME}/env-sit
+./bin/start-streaming-register-ebao.sh
 
 java -cp "${APP_HOME}/lib/pcr420669-load-1.0.jar:${APP_HOME}/lib/*" -Dprofile.active=env-sit com.transglobe.streamingetl.pcr420669.load.InitialLoadApp2 $1
 STATUS=$?	
