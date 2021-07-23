f5137cb15568a9a6caf42ab5e8b8d6f043c20acb
steven <steven@mylinux>
2021-07-23 13:51:36 +0800

    initial

--- pcr420669-load/src/main/resources/logminertable-T_STREAMING_ETL_HEALTH_CDC.sql
+++
@@ -1,5 +0,0 @@
-create table T_STREAMING_ETL_HEALTH_CDC
-(
-	cdc_time NUMBER(19, 0),
-	primary key (cdc_time)
-)
--- pcr420669-load/src/main/resources/logminertable-T_STREAMING_REGISTER.sql
+++
@@ -1,7 +0,0 @@
-create table T_STREAMING_REGISTER
-(
-	time NUMBER(19, 0),
-	current_scn NUMBER(19, 0),
-	remark VARCHAR2(1000),
-	primary key (time)
-)
