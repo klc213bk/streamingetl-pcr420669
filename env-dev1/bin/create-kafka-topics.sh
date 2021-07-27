c4b648fea5086c5b8fae487f9a0e142ffb2cbcc5
steven <steven@mylinux>
2021-07-27 12:11:23 +0800

    initial

---
+++ env-dev1/bin/create-kafka-topics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+KAFKA_HOME=/home/steven/kafka_2.13-2.7.0
+
+cd ${KAFKA_HOME}
+
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_policy_holder.0
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_insured_list.0
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_contract_bene.0
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_policy_holder_log.0
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_insured_list_log.0
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_contract_bene_log.0
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ebao.cdc.test_t_address.0
---
+++ env-dev1/bin/start-ignite.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+IGNITE_HOME=/home/steven/apache-ignite-2.9.1-bin
+APP_HOME=/home/steven/gitrepo/transglobe/streamingetl-pcr420669
+
+${IGNITE_HOME}/bin/ignite.sh ${APP_HOME}/env-dev1/config/ignite-config.xml
+
